2fca3029a0cb5249029dc4c87543aa571b5dc093
Eduardo1749 <dudu.evangelista1749@gmail.com>
2022-02-06 14:12:12 -0300

    endpoint findAll para Tecnico e base de dados atualizada

--- src/main/java/com/eduardo/helpdesk/resources/excptions/ResourceExcptionHandler.java
+++ src/main/java/com/eduardo/helpdesk/resources/excptions/ResourceExcptionHandler.java
@@ -1,7 +1,5 @@
 package com.eduardo.helpdesk.resources.excptions;
 
-import java.time.LocalDateTime;
-
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
--- src/main/java/com/eduardo/helpdesk/services/DBService.java
+++ src/main/java/com/eduardo/helpdesk/services/DBService.java
@@ -31,12 +31,33 @@ public class DBService {
 	public void instanciaDB() {
 		Tecnico tec1 = new Tecnico(null, "Valdir cezar", "63653230268", "valdir@mail.com", "123");
 		tec1.addPerfil(Perfil.ADMIN);
-		Cliente cli1 = new Cliente(null, "Linus Torvalds", "80527954780", "torvalds@mail.com", "123");
-		Chamado c1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 01", "Primeiro chamado", tec1, cli1);
+		Tecnico tec2 = new Tecnico(null, "Fabiano Pereira", "39252161031", "fabiano@mail.com", "123");
+		Tecnico tec3 = new Tecnico(null, "Daihana Ribeiro", "69171650008", "daihana@mail.com", "123");
+		Tecnico tec4 = new Tecnico(null, "Eduardo Santos", "33940429082", "eduardo@mail.com", "123");
+		tec4.addPerfil(Perfil.ADMIN);
+		Tecnico tec5 = new Tecnico(null, "Marcos Gabriel", "24281358056", "gabriel@mail.com", "123");
+		Tecnico tec6 = new Tecnico(null, "Victoria Luisa", "73459437049", "victoria@mail.com", "123");
 		
-		tecnicoRepository.saveAll(Arrays.asList(tec1));
-		clienteRepository.saveAll(Arrays.asList(cli1));
-		chamadoRepository.saveAll(Arrays.asList(c1));
+		
+		Cliente cli1 = new Cliente(null, "Linus Torvalds", "80527954780", "torvalds@mail.com", "123");
+		Cliente cli2 = new Cliente(null, "Vitor Lucas", "23204767094", "vitor@mail.com", "123");
+		Cliente cli3 = new Cliente(null, "Vitor Chrispim", "09267586084", "chrispim@mail.com", "123");
+		Cliente cli4 = new Cliente(null, "Rosilda Ribeiro", "12367145040", "rosilda@mail.com", "123");
+		Cliente cli5 = new Cliente(null, "Maria Adenilza", "67505437070", "nilza@mail.com", "123");
+		Cliente cli6 = new Cliente(null, "Liliane Santos", "14655423021", "liliane@mail.com", "123");
+		
+
+		Chamado c1 = new Chamado(null, Prioridade.MEDIA, Status.ANDAMENTO, "Chamado 01", "Primeiro chamado", tec1, cli1);
+		Chamado c2 = new Chamado(null, Prioridade.BAIXA, Status.ABERTO, "Chamado 02", "Segundo chamado", tec2, cli2);
+		Chamado c3 = new Chamado(null, Prioridade.ALTA, Status.ANDAMENTO, "Chamado 03", "Terceiro chamado", tec3, cli3);
+		Chamado c4 = new Chamado(null, Prioridade.MEDIA, Status.ENCERRADO, "Chamado 04", "Quarto chamado", tec4, cli4);
+		Chamado c5 = new Chamado(null, Prioridade.BAIXA, Status.ABERTO, "Chamado 05", "Quinto chamado", tec5, cli5);
+		Chamado c6 = new Chamado(null, Prioridade.ALTA, Status.ENCERRADO, "Chamado 06", "Sexto chamado", tec6, cli6);
+		
+		
+		tecnicoRepository.saveAll(Arrays.asList(tec1, tec2, tec3, tec4, tec5, tec6));
+		clienteRepository.saveAll(Arrays.asList(cli1, cli2, cli3, cli4, cli5, cli6));
+		chamadoRepository.saveAll(Arrays.asList(c1, c2, c3, c4, c5, c6));
 		
 	}
 }
