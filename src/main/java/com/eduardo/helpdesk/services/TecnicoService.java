52088a483e50adebd83b539434d04edbdf5ed8fa
Eduardo1749 <dudu.evangelista1749@gmail.com>
2022-02-07 15:44:32 -0300

    endpoint create para Tecnico

--- src/main/java/com/eduardo/helpdesk/services/TecnicoService.java
+++ src/main/java/com/eduardo/helpdesk/services/TecnicoService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.eduardo.helpdesk.domain.Tecnico;
+import com.eduardo.helpdesk.domain.dtos.TecnicoDTO;
 import com.eduardo.helpdesk.repositories.TecnicoRepository;
 import com.eduardo.helpdesk.services.exceptions.ObjectnotFoundException;
 
@@ -25,5 +26,11 @@ public class TecnicoService {
 		
 		return repository.findAll();
 	
+	}
+
+	public Tecnico create(TecnicoDTO objDTO) {
+	objDTO.setId(null);
+	Tecnico newObj = new Tecnico(objDTO); 
+	return repository.save(newObj);
 	}
 }
